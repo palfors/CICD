09cf2af03c08bcff0ce0a5527515d1ca4d132cbc
Peter Alfors <palfors@gmail.com>
2014-11-12 23:43:01 -0600

    added concordian test

---
+++ src/test/java/com/alforsconsulting/cicd/concordion/checkingAccountDepositTest.java
@@ -0,0 +1,22 @@
+package com.alforsconsulting.cicd.concordion;
+
+import com.alforsconsulting.cicd.CheckingAccount;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by palfors on 11/12/14.
+ */
+@RunWith(ConcordionRunner.class)
+public class checkingAccountDepositTest
+{
+
+    public double validateDeposit(double initialBalance, double depositAmount)
+    {
+        CheckingAccount account = new CheckingAccount(initialBalance);
+        account.deposit(depositAmount);
+
+        return account.getBalance();
+    }
+
+}
